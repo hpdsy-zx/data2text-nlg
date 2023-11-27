3788b95f64110503b422199eb30e34edeffe0b1a
hpdsy-zx <1073044176@qq.com>
2023-11-27 21:55:53 +0800

    2

--- slot_aligner/data_analysis(1).py
+++ slot_aligner/data_analysis(1).py
@@ -1,15 +1,17 @@
 from collections import Counter, OrderedDict
 import json
 import os
+import re
+import sys
 import pandas as pd
 
-from data2text_nlg.constants import SlotNameConversionMode
-from data2text_nlg.dataset_loaders.e2e import E2EDataset, E2ECleanedDataset
-from data2text_nlg.dataset_loaders.multiwoz import MultiWOZDataset
-from data2text_nlg.dataset_loaders.viggo import ViggoDataset
-from data2text_nlg.dataset_loaders.rnnlg import LaptopDataset
-from data2text_nlg.dataset_loaders.rnnlg import TVDataset
-from data2text_nlg.slot_aligner.slot_alignment import count_errors, find_alignment
+import data_loader
+from constants import SlotNameConversionMode
+from dataset_loaders.e2e import E2EDataset, E2ECleanedDataset
+from dataset_loaders.multiwoz import MultiWOZDataset
+from dataset_loaders.viggo import ViggoDataset
+from dataset_loaders.song import SongDataset
+from slot_aligner.slot_alignment import count_errors, find_alignment
 
 
 def align_slots(data_dir, filename, dataset_class, serialize_pos_info=False):
@@ -25,11 +27,10 @@ def align_slots(data_dir, filename, dataset_class, serialize_pos_info=False):
 
     # Load MRs and corresponding utterances
     df_data = pd.read_csv(os.path.join(data_dir, filename), header=0)
-    print(df_data.columns)
-    mrs_raw = df_data.iloc[:, 2].to_list()
+    mrs_raw = df_data.iloc[:, 0].to_list()
     mrs_processed = dataset_class.preprocess_mrs(
         mrs_raw, as_lists=True, lowercase=False, slot_name_conversion=SlotNameConversionMode.SPECIAL_TOKENS)
-    utterances = df_data.iloc[:, 10].to_list()
+    utterances = df_data.iloc[:, 1].to_list()
 
     for mr_as_list, utt in zip(mrs_processed, utterances):
         # Determine the positions of all slot mentions in the utterance
@@ -55,33 +56,31 @@ def score_slot_realizations(data_dir, predictions_file, dataset_class, num, slot
 
     # Load MRs and corresponding utterances
     df_data = pd.read_csv(os.path.join(data_dir, predictions_file), header=0)
-   # print(df_data.columns)
     mrs_raw = df_data.iloc[:, 0].to_list()
-
-    print(mrs_raw)
-    #break
     mrs_processed = dataset_class.preprocess_mrs(
         mrs_raw, as_lists=True, lowercase=False, slot_name_conversion=SlotNameConversionMode.SPECIAL_TOKENS)
     utterances = df_data.iloc[:, 1].fillna('').to_list()
-    print(utterances)
     lengths = []
     sacc = []
     for mr_as_list, utt in zip(mrs_processed, utterances):
         # Count the missing and hallucinated slots in the utterance
-        length = len(mr_as_list)-1
-        lengths.append(len(mr_as_list)-1)
+        length = len(mr_as_list) - 1
+        lengths.append(len(mr_as_list) - 1)
         num_errors, cur_incorrect_slots, cur_duplicate_slots, num_content_slots = count_errors(
             utt, mr_as_list, dataset_class.name, verbose=verbose)
         error_counts.append(num_errors)
-        sacc.append(100*((length-num_errors)/length))
+        sacc.append(100 * ((length - num_errors) / length))
         incorrect_slots.append(', '.join(cur_incorrect_slots))
         duplicate_slots.append(', '.join(cur_duplicate_slots))
         total_content_slots += num_content_slots
 
     # Save the MRs and utterances along with their slot error indications to a new CSV file
-    df_data['errors'+num] = error_counts
-    df_data['incorrect'+num] = incorrect_slots
-    df_data['duplicate'+num] = duplicate_slots
+    # df_data['errors'] = error_counts
+    # df_data['incorrect'] = incorrect_slots
+    # df_data['duplicate'] = duplicate_slots
+    df_data['errors' + num] = error_counts
+    df_data['incorrect' + num] = incorrect_slots
+    df_data['duplicate' + num] = duplicate_slots
     df_data["mr_len"] = lengths
     df_data[f"SACC{num}"] = sacc
 
@@ -100,7 +99,7 @@ def score_slot_realizations(data_dir, predictions_file, dataset_class, num, slot
     else:
         print(f'{round(100 * ser, 2)}%')
 
-    return ser, df_data
+    return ser,df_data
 
 
 def score_emphasis(dataset, filename):
@@ -300,7 +299,7 @@ def analyze_contrast_relations(dataset, filename):
         slot_before = None
         slot_after = None
 
-        for contrast_co``````````````````````````````````````nn in contrast_connectors:
+        for contrast_conn in contrast_connectors:
             contrast_pos = utt.find(contrast_conn)
             if contrast_pos >= 0:
                 slot_before = None
@@ -341,82 +340,60 @@ def analyze_contrast_relations(dataset, filename):
     filename_out = os.path.splitext(filename)[0] + ' [contrast relations].csv'
     new_df.to_csv(os.path.join(config.DATA_DIR, dataset, filename_out), index=False, encoding='utf8')
 
-def convert_format(input_path, file, text_col, outfile):
-    """Changes the format of our data to match this ssript."""
-
-    new_pd = pd.read_csv(os.path.join(input_path, file))
-    new_pd = new_pd
-    cols = list(new_pd.columns.values)
-    all_mrs = new_pd['mr'].tolist()
-    new_mrs = []
-    for mr in all_mrs:
-        print(mr)
-        print(mr.split(' | ',1)[0].split(' ')[0])
-        DA = mr.split(' | ',1)[0].split(' ')[0]
-        mrs = mr.split(' | ')[1:]
-        before = DA + '('
-        for m in mrs:
-            double = m.split(' =')
-            first = double[0]
-            second = double[1][1:]
-            before = before + first +'['+ second + '], '
-        before = before[:-2] + ')'
-        new_mrs.append(before)
-    #print(all_mrs)
-    new_pd['mr'] = new_mrs
-  #  print(len(new_mrs))
-    new_pd[['mr', text_col]].to_csv(os.path.join(input_path, outfile),index = False)
-
-
-def run_for_horizontal(input_path, file_name, convert, pd_file_output):
-    collect_df = []
-    list_of_errors = {"errors":[], "SACC":[], "text":[], "id":[]}
-
-    for i in range(1, 11):
-       # print(i)
-        convert_format(input_path, file_name, f'text{i}', f'{convert}_{i}.csv')
-        ser, df = score_slot_realizations(input_path, f'{convert}_{i}.csv', ViggoDataset, f'{i}',
-                                          slot_level=True)
-        errors = df[f'errors{i}'].tolist()
-        sacc = df[f"SACC{i}"].tolist()
-        indexes = [i for i, v in enumerate(sacc)]
-        list_of_errors["text"].extend(df[f"text{i}"].tolist())
-        list_of_errors["errors"].extend(errors)
-        list_of_errors["SACC"].extend(sacc)
-        list_of_errors["id"].extend(indexes)
-        collect_df.append(df)
-    pd.concat(collect_df, axis=1, ignore_index=False).to_csv(f'{pd_file_output}_each_errors.csv', index=False)
-    pd.DataFrame.from_dict(list_of_errors).to_csv(f'{pd_file_output}_only.csv', index=False)
-
 
 if __name__ == '__main__':
-    from os import listdir
-    from os.path import isfile, join
-    #print(os.listdir("data2text_nlg\data\\video_game\\bulk"))
-    #input_path = "~\PycharmProjects\Jurassic\data2text_nlg\data\\tv_rnnlg"
-    input_path = '~\PycharmProjects\Jurassic\data2text_nlg\data\\viggo'
-    #onlyfiles = [f for f in os.listdir() if os.path.isfile(os.path.join(input_path, f))]
-   # files = [filenames for (dirpath, dirnames, filenames) in os.walk(input_path) ]
-    #print(files)
-    #C:\Users\arami\PycharmProjects\Jurassic\liren_s code\experiments\ex19\viggo_test_overgen_5_output.csv
+    #align_slots(r'/d/Git/data2text-nlg/data/rest_e2e', 'devset.csv', E2EDataset, serialize_pos_info=False)
 
-    file_name = "chatgpt_rf2da.csv"
-    # #\
-    convert_format(input_path, file_name, f'text', f'{file_name}_each.csv')
+    input_path = 'D:\data2text-nlg\data\song'
+    file_name = '1_hops_final_ranked - 1_hops_final_ranked.csv'
 
-    ser, df = score_slot_realizations(input_path, f'{file_name}_each.csv', ViggoDataset, f'',  slot_level=True)
-    #1111111111111111111111111111111111111111111111111111111111
-    # # run_for_horizontal(input_path+"1prompt_callisonDIAL", "viggo-test-specific1_newCallison.csv", '1_newCallison', '1_newCallison')
-    # # run_for_horizontal(input_path + "5prompt_callisonDIAL", "viggo-test-specific5_newCallison.csv", '5_newCallison',
-    # #                    '5_newCallison')
-    # # run_for_horizontal(input_path + "10prompt_callisonDIAL", "viggo-test-specific10_newCallison.csv", '10_newCallison',
-    # #                    '10_newCallison')
-    # # run_for_horizontal(input_path + "2prompt_callisonDIAL", "2_prompt_callisonNEW_output.csv", '2_newCallison',
-    # #                    '2_newCallison')
-    # # files = ['viggo-test-specific5_newCallison - with DA.csv', 'viggo-test-specific10_newCallison - with DA.csv','viggo-test-specific1_newCallison (2) - with DA.csv', '2_prompt_callisonNEW_output - with DA.csv']
-    # #
-    # for i in files:
-    #     print(i.split(" - with DA.csv")[0])
-    #     convert_format(input_path, i, f'text', f'{i.split(" - with DA.csv")[0]}_each.csv')
-    #     ser, df = score_slot_realizations(input_path, f'{i.split(" - with DA.csv")[0]}_each.csv', ViggoDataset, f'all',
-    #                                     slot_level=True)
+    ser,df = score_slot_realizations(input_path,file_name,SongDataset,f'',slot_level=True)
+
+    '''
+    calculate animals ser
+    '''
+    # input_path = 'D:\data2text-nlg\data\\animals'
+    # file_name = 'chatgpt_alldas.csv'
+    #
+    # ser = score_slot_realizations(input_path,file_name,SongDataset,f'',slot_level=True)
+
+
+
+    # ----
+
+    # score_slot_realizations(r'/d/Git/data2text-nlg/data/multiwoz',
+    #                         'valid.csv', MultiWOZDataset, slot_level=True)
+    # score_slot_realizations(r'/d/Git/data2text-nlg/data/video_game',
+    #                         'valid.csv', ViggoDataset, slot_level=True)
+    # score_slot_realizations(r'/d/Git/data2text-nlg/predictions_baselines/DataTuner/video_game',
+    #                         'systemFc.csv', ViggoDataset, slot_level=True)
+
+    # score_slot_realizations(r'/d/Git/data2text-nlg/predictions/video_game/finetuned_verbalized_slots/bart-base_lr_1e-5_bs_32_wus_100_run3/beam_search_20',
+    #                         f'epoch_18_step_160_beam_search_reranked_att_1.0.csv', ViggoDataset, slot_level=True)
+    # score_slot_realizations(r'/d/Git/data2text-nlg/predictions/rest_e2e/finetuned_verbalized_slots/bart-base_lr_1e-5_bs_32_wus_500_run3/beam_search_20',
+    #                         f'epoch_17_step_2629_beam_search_reranked_att_1.0.csv', E2EDataset, slot_level=True)
+    # score_slot_realizations(r'/d/Git/data2text-nlg/predictions/multiwoz/finetuned_verbalized_slots/bart-base_lr_1e-5_bs_32_wus_500_run4',
+    #                         'epoch_18_step_1749_beam_search_reranked_att_2.0.csv', MultiWOZDataset, slot_level=True)
+
+    # for thr in range(2, 5):
+    #     score_slot_realizations(r'/d/Git/data2text-nlg/predictions/video_game/finetuned_verbalized_slots/bart-large_lr_4e-6_bs_16_wus_500_run1/att_guidance_eos_03',
+    #                             f'epoch_13_step_319_beam_search_reranked_1.0_thr_{thr:02d}.csv', ViggoDataset, slot_level=True)
+    # for thr in range(2, 5):
+    #     score_slot_realizations(r'/d/Git/data2text-nlg/predictions/rest_e2e/finetuned_verbalized_slots/t5-base_lr_3e-5_bs_32_wus_100_run1/att_guidance_eos_03',
+    #                             f'epoch_17_step_1315_beam_search_reranked_1.0_thr_{thr:02d}.csv', E2EDataset, slot_level=True)
+
+    # ----
+
+    # score_emphasis('predictions-rest_e2e_stylistic_selection/devset', 'predictions RNN (4+4) augm emph (reference).csv')
+
+    # ----
+
+    # predictions_dir = 'predictions rest_e2e (emphasis+contrast)'
+    # predictions_file = 'predictions TRANS emphasis+contrast, train single, test combo extra (23.2k iter).csv'
+    #
+    # score_emphasis(predictions_dir, predictions_file)
+    # score_contrast(predictions_dir, predictions_file)
+
+    # ----
+
+    # analyze_contrast_relations('rest_e2e', 'devset_e2e.csv')
